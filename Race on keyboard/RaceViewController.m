b7d5c23e107c3af63b1089ad8210fd7ad2f2fd24
Semen Matsepura <a3:ac:3e:63:22:72:39:39:7f:83:fc:f2:77:42:fc:a9>
2015-08-15 01:53:55 +0200

    check the cars and make random, refactor cars

--- Race on keyboard/RaceViewController.m
+++ Race on keyboard/RaceViewController.m
@@ -16,6 +16,12 @@
 @property (weak, nonatomic) IBOutlet UISlider *progressRace;
 @property (weak, nonatomic) IBOutlet UISlider *opponentSliderOne;
 @property (weak, nonatomic) IBOutlet UISlider *opponentSliderTwo;
+@property (assign , nonatomic) NSInteger numberOfCar;
+
+//проверка, чтобы не попались одинаковые машинки
+@property (assign , nonatomic) NSInteger checkRepeatNumberOfCar;
+@property (assign , nonatomic) NSInteger checkRepeatNumberOfCarTwice;
+@property (assign, nonatomic) NSInteger checkAboutRepeat;
 
 @property (nonatomic) Race* raceProperty;
 
@@ -28,6 +34,7 @@
 -(void)setup{
     self.raceProperty = [[Race alloc] init];
     [self.enterRaceTextField becomeFirstResponder];
+    self.progressRace.backgroundColor = [UIColor colorWithRed:0 green:0 blue:0.7 alpha:0.03];
     [self setupMoreSlider];
     [self.raceProperty setUpTextInRace:self.textView AndMakeMaxValueOfSlider:self.progressRace];
     self.progressRace.value = 0;
@@ -35,7 +42,19 @@
 }
 
 -(void)setupSider:(UISlider *)slider{
-    [slider setThumbImage:[UIImage imageNamed:[NSString stringWithFormat:@"car%d.png", arc4random_uniform(48) + 1]] forState:UIControlStateNormal];
+    //проверка, чтобы не попались одинаковые машинки
+    self.numberOfCar = arc4random_uniform(48) + 1;
+    NSLog(@"random%ld", (long)self.numberOfCar);
+    if (self.numberOfCar != self.checkRepeatNumberOfCar && self.numberOfCar != self.checkRepeatNumberOfCarTwice) {
+        [slider setThumbImage:[UIImage imageNamed:[NSString stringWithFormat:@"car%ld.png", (long)self.numberOfCar]] forState:UIControlStateNormal];
+        self.checkRepeatNumberOfCar = self.numberOfCar;
+        if (self.checkAboutRepeat < 1) {
+            self.checkAboutRepeat++;
+            self.checkRepeatNumberOfCarTwice = self.numberOfCar;
+        }
+    }else{
+        [self setupSider:slider];
+    }
     slider.minimumTrackTintColor = [UIColor clearColor];
     slider.maximumTrackTintColor = [UIColor clearColor];
     slider.userInteractionEnabled = NO;
