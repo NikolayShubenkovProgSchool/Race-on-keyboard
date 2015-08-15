f84deec6828c34d90d65d9b91d544e920e8f396c
Semen Matsepura <a3:ac:3e:63:22:72:39:39:7f:83:fc:f2:77:42:fc:a9>
2015-08-15 22:30:33 +0200

    half of choose done

--- Race on keyboard/PlayerCarChooseViewController.m
+++ Race on keyboard/PlayerCarChooseViewController.m
@@ -11,6 +11,8 @@
 @interface PlayerCarChooseViewController ()
 
 @property (weak) UIScrollView *scrollView;
+@property (weak, nonatomic) IBOutlet UIButton *firstCar;
+@property (weak, nonatomic) IBOutlet UIButton *secondCar;
 
 
 
@@ -25,9 +27,11 @@
 }
 
 -(void)test{
+#warning  завтра разобрать тут!
     NSInteger x = 40;
     NSInteger y = 60;
     NSInteger c = 0;
+    NSInteger d = 1;
     for (int i = 0; i < 4; i++) {
         if (c == 2) {
             y = y + 60;
@@ -38,14 +42,15 @@
     [button setTitle:@"" forState:UIControlStateNormal];
     [button sizeToFit];
     
-    button.frame = CGRectMake(320/2 , 536/2, 90, 90);
+    
+    button.frame = CGRectMake(self.firstCar.frame.origin.x + 40 , self.firstCar.frame.origin.y + 40, 90, 90);
     button.center = CGPointMake(40 + x, y);
     
     // Add an action in current code file (i.e. target)
     [button addTarget:self action:@selector(buttonPressed:)
      forControlEvents:UIControlEventTouchUpInside];
-    [button setBackgroundImage:[UIImage imageNamed:@"car1.png"] forState:UIControlStateNormal];
-    
+    [button setBackgroundImage:[UIImage imageNamed:[NSString stringWithFormat:@"car%ld.png", d]] forState:UIControlStateNormal];
+        d++;
     [self.view addSubview:button];
         c++;
         x = x+100;
